2c1d163d727aebd9a32043e0a42e681d568bf0fa
姜华 <jh.jlu@outlook.com>
2016-08-25 17:57:33 +0800

    first commit

---
+++ src/com/fh/service/feature/charts/impl/ChartsService.java
@@ -0,0 +1,89 @@
+package com.fh.service.feature.charts.impl;
+
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import com.fh.dao.DaoSupport;
+import com.fh.entity.Page;
+import com.fh.util.PageData;
+import com.fh.service.feature.charts.ChartsManager;
+
+/**
+ * 说明： 图表处理
+ * 创建人：JH
+ * 创建时间：2016-03-07
+ * @version
+ */
+@Service("chartsService")
+public class ChartsService implements ChartsManager{
+
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+
+	/**新增
+	 * @param pd
+	 * @throws Exception
+	 */
+	@Override
+	public void save(PageData pd)throws Exception{
+		dao.save("ChartsMapper.save", pd);
+	}
+
+	/**删除
+	 * @param pd
+	 * @throws Exception
+	 */
+	@Override
+	public void delete(PageData pd)throws Exception{
+		dao.delete("ChartsMapper.delete", pd);
+	}
+
+	/**修改
+	 * @param pd
+	 * @throws Exception
+	 */
+	@Override
+	public void edit(PageData pd)throws Exception{
+		dao.update("ChartsMapper.edit", pd);
+	}
+
+	/**列表
+	 * @param page
+	 * @throws Exception
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<PageData> list(Page page)throws Exception{
+		return (List<PageData>)dao.findForList("ChartsMapper.datalistPage", page);
+	}
+
+	/**列表(全部)
+	 * @param pd
+	 * @throws Exception
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<PageData> listAll(PageData pd)throws Exception{
+		return (List<PageData>)dao.findForList("ChartsMapper.listAll", pd);
+	}
+
+	/**通过id获取数据
+	 * @param pd
+	 * @throws Exception
+	 */
+	@Override
+	public PageData findById(PageData pd)throws Exception{
+		return (PageData)dao.findForObject("ChartsMapper.findById", pd);
+	}
+
+	/**批量删除
+	 * @param ArrayDATA_IDS
+	 * @throws Exception
+	 */
+	@Override
+	public void deleteAll(String[] ArrayDATA_IDS)throws Exception{
+		dao.delete("ChartsMapper.deleteAll", ArrayDATA_IDS);
+	}
+
+}
+
